847aae234a2f881107b9c5f9180562949be66ef4
SerhioToretto <serjikus22@rambler.ru>
2024-06-21 02:17:53 +0600

    feature/2 пр фиксы

--- app/services/MtkGameService.ts
+++ app/services/MtkGameService.ts
@@ -2,7 +2,6 @@ import BaseGameService, {HasDailyReward, HasEnergyRecharge, HasTap} from '#servi
 import telegramConfig from '#config/telegram';
 import { NormalizedOptions } from '../../types/ky.js';
 import { URLSearchParams } from 'node:url';
-import telegram from "#config/telegram";
 
 export default class MtkGameService extends BaseGameService
     implements HasTap, HasDailyReward, HasEnergyRecharge {
@@ -80,29 +79,21 @@ export default class MtkGameService extends BaseGameService
     }
 
     public async collectDaily(): Promise<void> {
-        if (!this.token) {
-            throw new Error('Clicker token not found');
-        }
-
         const searchParams = new URLSearchParams();
-        searchParams.set('userId', `${telegram.api.userId}`);
+        searchParams.set('userId', `${telegramConfig.api.userId}`);
 
-        return await this.httpClient.post('api/user/collectDaily', {
+        await this.httpClient.post('api/user/collectDaily', {
             searchParams,
-        }).json();
+        });
     }
 
     public async energyReset(): Promise<void> {
-        if (!this.token) {
-            throw new Error('Clicker token not found');
-        }
-
         const searchParams = new URLSearchParams();
-        searchParams.set('userId', `${telegram.api.userId}`);
+        searchParams.set('userId', `${telegramConfig.api.userId}`);
 
-        return await this.httpClient.post('api/user/resetEnergy', {
+        await this.httpClient.post('api/user/resetEnergy', {
             searchParams,
-        }).json();
+        });
     }
 
 };
