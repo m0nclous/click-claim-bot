b6e015d058c97aee6dc5e4e69907efb2d6ea0e73
m0nclous <m0nclous@vk.com>
2024-07-09 02:13:28 +0600

    Добавлена проверка на положительное число

--- helpers/timer.ts
+++ helpers/timer.ts
@@ -1,1 +1,7 @@
-export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (ms: number) => {
+    if (ms < 0) {
+        throw new TypeError('ms должен быть положительным');
+    }
+
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
