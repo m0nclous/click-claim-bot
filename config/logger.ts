e3f4d090a18fb37d21e65010d2d81ab8673e6f9d
m0nclous <m0nclous@vk.com>
2024-07-12 03:10:38 +0600

    #69 Добавлен redis password для production (#70)

--- config/logger.ts
+++ config/logger.ts
@@ -3,29 +3,31 @@ import app from '@adonisjs/core/services/app';
 import { defineConfig, targets } from '@adonisjs/core/logger';
 import { readFileSync } from 'node:fs';
 
-const elasticsearchConfig = app.inProduction ? {
-    target: 'pino-elasticsearch',
-    level: env.get('LOG_LEVEL'),
-    options: {
-        node: `https://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-        esVersion: 8,
-        auth: {
-            username: 'elastic',
-            password: env.get('ELASTIC_PASSWORD'),
-        },
-        tls: {
-            ca: readFileSync('./http_ca.crt'),
-            rejectUnauthorized: false,
-        },
-    },
-} : {
-    target: 'pino-elasticsearch',
-    level: env.get('LOG_LEVEL'),
-    options: {
-        node: `http://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-        esVersion: 8,
-    },
-};
+const elasticsearchConfig = app.inProduction
+    ? {
+          target: 'pino-elasticsearch',
+          level: env.get('LOG_LEVEL'),
+          options: {
+              node: `https://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
+              esVersion: 8,
+              auth: {
+                  username: 'elastic',
+                  password: env.get('ELASTIC_PASSWORD'),
+              },
+              tls: {
+                  ca: readFileSync('./http_ca.crt'),
+                  rejectUnauthorized: false,
+              },
+          },
+      }
+    : {
+          target: 'pino-elasticsearch',
+          level: env.get('LOG_LEVEL'),
+          options: {
+              node: `http://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
+              esVersion: 8,
+          },
+      };
 
 const loggerConfig = defineConfig({
     default: 'app',
