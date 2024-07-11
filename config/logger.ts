4742f041216271a5f3d671642fdefc75d9182a58
m0nclous <m0nclous@vk.com>
2024-07-12 03:08:07 +0600

    fix elastic config gameServiceRequest production

--- config/logger.ts
+++ config/logger.ts
@@ -3,6 +3,30 @@ import app from '@adonisjs/core/services/app';
 import { defineConfig, targets } from '@adonisjs/core/logger';
 import { readFileSync } from 'node:fs';
 
+const elasticsearchConfig = app.inProduction ? {
+    target: 'pino-elasticsearch',
+    level: env.get('LOG_LEVEL'),
+    options: {
+        node: `https://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
+        esVersion: 8,
+        auth: {
+            username: 'elastic',
+            password: env.get('ELASTIC_PASSWORD'),
+        },
+        tls: {
+            ca: readFileSync('./http_ca.crt'),
+            rejectUnauthorized: false,
+        },
+    },
+} : {
+    target: 'pino-elasticsearch',
+    level: env.get('LOG_LEVEL'),
+    options: {
+        node: `http://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
+        esVersion: 8,
+    },
+};
+
 const loggerConfig = defineConfig({
     default: 'app',
 
@@ -18,30 +42,7 @@ const loggerConfig = defineConfig({
             transport: {
                 targets: targets()
                     .pushIf(!app.inProduction, targets.pretty())
-                    .pushIf(!app.inProduction, {
-                        target: 'pino-elasticsearch',
-                        level: env.get('LOG_LEVEL'),
-                        options: {
-                            node: `http://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-                            esVersion: 8,
-                        },
-                    })
-                    .pushIf(app.inProduction, {
-                        target: 'pino-elasticsearch',
-                        level: env.get('LOG_LEVEL'),
-                        options: {
-                            node: `https://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-                            esVersion: 8,
-                            auth: {
-                                username: 'elastic',
-                                password: env.get('ELASTIC_PASSWORD'),
-                            },
-                            tls: {
-                                ca: readFileSync('./http_ca.crt'),
-                                rejectUnauthorized: false,
-                            },
-                        },
-                    })
+                    .push(elasticsearchConfig)
                     .toArray(),
             },
         },
@@ -52,14 +53,7 @@ const loggerConfig = defineConfig({
             transport: {
                 targets: targets()
                     .pushIf(!app.inProduction, targets.pretty())
-                    .push({
-                        target: 'pino-elasticsearch',
-                        level: env.get('LOG_LEVEL'),
-                        options: {
-                            node: 'http://elasticsearch:9200',
-                            esVersion: 8,
-                        },
-                    })
+                    .push(elasticsearchConfig)
                     .toArray(),
             },
         },
