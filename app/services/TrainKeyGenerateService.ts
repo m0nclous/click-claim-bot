bdb8a000a7c5e23fe1a2387d29d434ee33b91d2b
m0nclous <m0nclous@vk.com>
2024-08-07 02:22:21 +0600

    TrainKeyGenerateProvider

---
+++ app/services/TrainKeyGenerateService.ts
@@ -0,0 +1,19 @@
+import { BaseKeyGenerateService } from '#services/BaseKeyGenerateService';
+
+export class TrainKeyGenerateService extends BaseKeyGenerateService {
+    public constructor(protected clientId: string) {
+        super(clientId);
+    }
+
+    protected getAppToken(): string {
+        return '82647f43-3f87-402d-88dd-09a90025313f';
+    }
+
+    protected getPromoId(): string {
+        return 'c4480ac7-e178-4973-8061-9ed5b2e17954';
+    }
+
+    protected getEventType(): string {
+        return 'hitStatue';
+    }
+}
---
+++ providers/TrainKeyGenerateProvider.ts
@@ -0,0 +1,24 @@
+import { ApplicationService } from '@adonisjs/core/types';
+import { TrainKeyGenerateService } from '#services/TrainKeyGenerateService';
+
+declare module '@adonisjs/core/types' {
+    // noinspection JSUnusedGlobalSymbols
+    export interface ContainerBindings {
+        trainKeyGenerate: TrainKeyGenerateService;
+    }
+}
+
+export default class TrainKeyGenerateProvider {
+    constructor(protected app: ApplicationService) {}
+
+    // noinspection JSUnusedGlobalSymbols
+    public async register(): Promise<void> {
+        this.app.container.bind('trainKeyGenerate', async () => {
+            const clientId: string = crypto.randomUUID();
+
+            const { TrainKeyGenerateService } = await import('#services/TrainKeyGenerateService');
+
+            return new TrainKeyGenerateService(clientId);
+        });
+    }
+}
