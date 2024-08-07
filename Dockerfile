093e879adf878ea5b0ece410ab14132bbfde0d23
m0nclous <m0nclous@vk.com>
2024-08-08 01:02:57 +0600

    Удалены все dev контейнеры

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,28 @@
-# syntax=docker/dockerfile:experimental
-
-FROM node:20.10.0-alpine as base
-RUN apk add --no-cache python3 make g++
+FROM node:20.10.0-alpine AS base
 WORKDIR /app
 
+ENV HOST=0.0.0.0
+ENV PORT=80
+EXPOSE 80
+
 # All deps stage
-FROM base as deps
+FROM base AS deps
 ADD package.json package-lock.json ./
 RUN npm ci
 
 # Production only deps stage
-FROM base as production-deps
+FROM base AS production-deps
 ADD package.json package-lock.json ./
 RUN npm ci --omit=dev
 
 # Build stage
-FROM base as build
+FROM base AS build
 COPY --from=deps /app/node_modules /app/node_modules
 ADD . .
-RUN node ace build --ignore-ts-errors
-COPY .env /app/build
-COPY .docker/elasticsearch/cert/http_ca.crt /app/build
-
-# Development stage
-FROM base as development
-COPY --from=deps /app/node_modules /app/node_modules
-ADD . .
-CMD ["node", "ace", "--inspect=0.0.0.0", "serve", "--watch"]
+RUN node ace build
 
 # Production stage
-FROM base as production
+FROM base
 COPY --from=production-deps /app/node_modules /app/node_modules
 COPY --from=build /app/build /app
 CMD ["node", "./bin/server.js"]
-
-# Production scheduler stage
-FROM production as production-scheduler
-CMD ["node", "ace", "scheduler:run"]
--- config/logger.ts
+++ config/logger.ts
@@ -1,33 +1,6 @@
 import env from '#start/env';
 import app from '@adonisjs/core/services/app';
 import { defineConfig, targets } from '@adonisjs/core/logger';
-import { readFileSync } from 'node:fs';
-
-const elasticsearchConfig = app.inProduction
-    ? {
-          target: 'pino-elasticsearch',
-          level: env.get('LOG_LEVEL'),
-          options: {
-              node: `https://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-              esVersion: 8,
-              auth: {
-                  username: 'elastic',
-                  password: env.get('ELASTIC_PASSWORD'),
-              },
-              tls: {
-                  ca: readFileSync('./http_ca.crt'),
-                  rejectUnauthorized: false,
-              },
-          },
-      }
-    : {
-          target: 'pino-elasticsearch',
-          level: env.get('LOG_LEVEL'),
-          options: {
-              node: `http://${env.get('ELASTIC_HOST')}:${env.get('ELASTIC_PORT')}`,
-              esVersion: 8,
-          },
-      };
 
 const loggerConfig = defineConfig({
     default: 'app',
@@ -44,7 +17,6 @@ const loggerConfig = defineConfig({
             transport: {
                 targets: targets()
                     .pushIf(!app.inProduction, targets.pretty())
-                    .push(elasticsearchConfig)
                     .toArray(),
             },
         },
@@ -55,7 +27,6 @@ const loggerConfig = defineConfig({
             transport: {
                 targets: targets()
                     .pushIf(!app.inProduction, targets.pretty())
-                    .push(elasticsearchConfig)
                     .toArray(),
             },
         },
@@ -66,7 +37,6 @@ const loggerConfig = defineConfig({
             transport: {
                 targets: targets()
                     .pushIf(!app.inProduction, targets.pretty())
-                    .push(elasticsearchConfig)
                     .toArray(),
             },
         },
